f7df976111253dca21778b1000df88094340c574
Sainul Abid <sainulabidofficial@gmail.com>
2023-12-15 00:54:48 +0530

    patch

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,23 @@
-const mongoClient = require('mongodb').MongoClient
+const mongoClient = require("mongodb").MongoClient;
 
-const state ={
-db:null
-}
+const state = {
+  db: null,
+};
 
-module.exports.connect= function(done) {
-    const url=process.env.MONGO_URI
-    const dbname='shoppingCart'
-    mongoClient.connect(url).then((client)=>{
-        
-        state.db=client.db(dbname)
-       
-       
-        done()
-    }).catch((err)=>{
-        return done(err)
+module.exports.connect = function (done) {
+  const url = process.env.MONGO_URI;
+  const dbname = "shoppingCart";
+  mongoClient
+    .connect(url)
+    .then((client) => {
+      state.db = client.db(dbname);
+
+      done();
     })
-}
-module.exports.get = ()=>{
-    return  state.db
-}
+    .catch((err) => {
+      return done(err);
+    });
+};
+module.exports.get = () => {
+  return state.db;
+};
